b3781919f952da93e39625d9a95e5b42e12fcbb8
DeBraid <de.braid@gmail.com>
2015-10-17 20:34:06 -0400

    cleaned up comments

--- contentscript.js
+++ contentscript.js
@@ -1,12 +1,4 @@
-// var s = document.createElement('script');
-// s.src = chrome.extension.getURL('cleanertweetdeck.js');
-// (document.head||document.documentElement).appendChild(s);
-// s.onload = function() {
-//     s.parentNode.removeChild(s);
-// };
-
 var s = document.createElement('script');
-// TODO: add "cleanertweetdeck.js" to web_accessible_resources in manifest.json
 s.src = chrome.extension.getURL('cleanertweetdeck.js');
 s.onload = function() {
 	console.log('loading content script');
